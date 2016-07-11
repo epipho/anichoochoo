1542ff18c7a9b38e97aa75c13b51b42007b2eee9
epipho <hiwrac@gmail.com>
2016-07-11 02:47:48 -0400

    Keep cell-based state. Move horses/turtles

--- drawframe.py
+++ drawframe.py
@@ -13,6 +13,7 @@ NUMROWS = 6
 BLOCK_SIDE = int(SCREEN_HEIGHT/NUMROWS)
 BLOCK_SIZE = (BLOCK_SIDE,BLOCK_SIDE)
 ROWS = [BLOCK_SIDE * x for x in range(NUMROWS)]
+NUMCOLS = int(SCREEN_WIDTH/BLOCK_SIDE)
 FRAME_COUNT = 160
 
 choochoo = Image.open("images/choochoo.png").resize(BLOCK_SIZE)
@@ -26,43 +27,40 @@ sun = Image.open("images/sun.png").resize(BLOCK_SIZE)
 moon = Image.open("images/moon.png").resize(BLOCK_SIZE)
 blank = Image.new("RGBA",BLOCK_SIZE,(0,0,0,0))
 
+# chance the object will move (0 to 1) in the R,L,U,D directions
+# objects will never move into other scenery
+MOVABLES = [
+    [horse, 0.25, 0.5,0.2,0.2],
+    [turtle, 0.1, 0.1, 0.1, 0.1]
+]
+
+# List of random scenery
+SCENERY = [cactus,cactus,palm,palm,horse,turtle]
+# Rows that can contain scenery
+SCENERY_ROWS = [1,2,4,5]
+
 def main():
 
-    # "static" holds the elements that do not change throughout the animation.
-    # That includes the train and its cars.
+    # cells holds the current state of the world
+    cells = [None] * NUMROWS
+    for i in range(0,NUMROWS):
+        cells[i] = [None] * NUMCOLS
 
-    static = Image.new('RGBA',SCREEN_SIZE,(0,0,0,0))
 
-    choochx = int(BLOCK_SIDE * 6)
-    choochy = ROWS[3]
-    place_engine(static,choochx,choochy)
+    # generate train
+    choochx = 6
+    choochy = 3
 
     train_length = 7
+    train = []
 
-    carx = choochx
-
+    train.append(choochoo)
     for car in range(train_length):
-        carx = carx + BLOCK_SIDE
-        place_car(static,carx,choochy)
+        cars = [redcar,greencar]
+        train.append(random.choice(cars))
 
-    # "sky_row" is a long strip that has one sun and moon, advancing slowly.
-
-    sky_row = Image.new('RGBA',(2 * SCREEN_WIDTH,BLOCK_SIDE),(0,0,0,0))
-    sky_row.paste(sun,(SCREEN_WIDTH,0),sun)
-    sky_row.paste(moon,(0,0),moon)
-
-    # Each item in the "minutes" list is a frame on which the sky_row advances.
-
-    minutes = [int(FRAME_COUNT/40 * _) for _ in range(1,40)] 
-
-    sky_offset = SCREEN_WIDTH
-
-    # Start with one frame full of scenery, and set the offset to zero. It'll create more along the way.
-
-    scenery = Image.new('RGBA',SCREEN_SIZE,(0,0,0,0))
-    for row in [ROWS[1],ROWS[2],ROWS[4],ROWS[5]]:
-        place_scenery(scenery,row)
-    offset = 0
+    # generate initial scenery
+    generate_scenery(cells, SCENERY_ROWS)
 
     frame_number = 0
 
@@ -72,49 +70,100 @@ def main():
 
         render = Image.new('RGBA',SCREEN_SIZE,bgcolor)
 
-        bounce = random.randint(-4,4)
-        render.paste(static,(0,bounce),static)
+        # draw all cells
+        for i, r in enumerate(cells):
+            for j, c in enumerate(r):
+                if c != None:
+                    render.paste(c,(j*BLOCK_SIDE, i*BLOCK_SIDE), c)
 
-        render.paste(sky_row,(-sky_offset,0),sky_row)
-        if frame_number in minutes:
-            sky_offset -= BLOCK_SIDE
+        # draw the sky
+        draw_sky(render, frame_number)
 
-        if offset == 0:
-            scenery = scenery.crop((0,0,SCREEN_WIDTH,SCREEN_HEIGHT))
-            new_scene = Image.new('RGBA',SCREEN_SIZE,(0,0,0,0))
-            for row in [ROWS[1],ROWS[2],ROWS[4],ROWS[5]]:
-                place_scenery(new_scene,row)
-            combined_scene = Image.new('RGBA',(2 * SCREEN_WIDTH,SCREEN_HEIGHT),(0,0,0,0))
-            combined_scene.paste(new_scene,(0,0),new_scene)
-            combined_scene.paste(scenery,(SCREEN_WIDTH,0),scenery)
-            scenery = combined_scene
-            offset = SCREEN_WIDTH
+        # draw the train
+        draw_train(render, train, choochx, choochy)
 
-        render.paste(scenery,(-offset,0),scenery)
+        # scroll and move scenery
+        update_scenery(cells, SCENERY_ROWS)
         
         new_filename = "slides/img" + format(frame_number,"04d") + ".png"
         print("rendering frame " + str(frame_number) + " as " + new_filename)
         render.save(new_filename)
 
-        offset -= BLOCK_SIDE
         frame_number += 1
 
+def draw_sky(canvas,frame):
+    # how often sky moves (in frames)
+    SKY_MOVE_FRAME = FRAME_COUNT/40
+    sky_pos = int(frame / SKY_MOVE_FRAME) % (2 * NUMCOLS)
+    sky_img = sun
+    if sky_pos > NUMCOLS:
+        sky_pos -= NUMCOLS
+        sky_img = moon
+    canvas.paste(sky_img, (sky_pos*BLOCK_SIDE, 0), sky_img)
 
-def place_engine(canvas,x,y):
-    canvas.paste(choochoo,(x,y),choochoo)
+def draw_train(canvas, train, x, y):
+    bounce = random.randint(-4,4)
+    for i, car in enumerate(train):
+        canvas.paste(car,((x+i)*BLOCK_SIDE,y*BLOCK_SIDE+bounce),car)
 
-def place_car(canvas,x,y):
-    cars = [redcar,greencar]
-    car = random.choice(cars)
-    canvas.paste(car,(x,y),car)
+def generate_scenery(cells, rows):
+    for r in rows:
+        for c in range(0, NUMCOLS):
+            if random.randint(1,10) == 1:
+                cells[r][c] = random.choice(SCENERY)
 
-def place_scenery(canvas,y):
-    x = 0    
-    for block in range(int(canvas.width)):
+
+def update_scenery(cells, rows):
+    # move any movable scene objects
+    for r, row in enumerate(cells):
+        for c, obj in enumerate(row):
+            if obj != None:
+                # check if movable
+                for m in MOVABLES:
+                    if m[0] == obj:
+                        move_object(cells, rows, r, c, m)
+
+    for r in rows:
+        # move all scenery to the right
+        for c in range(NUMCOLS-1, 0, -1):
+            cells[r][c] = cells[r][c-1]
+
+        # spawn new scenery
+        cells[r][0] = None
         if random.randint(1,10) == 1:
-            thing = random.choice([cactus,cactus,palm,palm,horse,turtle])
-            canvas.paste(thing,(x,y),thing)
-        x += BLOCK_SIDE
+            cells[r][0] = random.choice(SCENERY)
+
+def move_object(cells, rows, r, c, move_def):
+    # move only once, randomly
+    moves = ["R", "L", "U", "D"]
+    random.shuffle(moves)
+
+    for m in moves:
+        # move right
+        if m == "R":
+            if c < NUMCOLS-1 and random.random() < move_def[1] and cells[r][c+1] == None:
+                cells[r][c+1] = cells[r][c]
+                cells[r][c] = None
+                return
+        # move left?
+        if m == "L":
+            if c > 0 and random.random() < move_def[2] and cells[r][c-1] == None:
+                cells[r][c-1] = cells[r][c]
+                cells[r][c] = None
+                return
+        # move up?
+        if m == "U":
+            if r > 0 and random.random() < move_def[3] and r-1 in rows and cells[r-1][c] == None:
+                cells[r-1][c] = cells[r][c]
+                cells[r][c] = None
+                return
+        # move down?
+        if m == "D":
+            if r < NUMROWS-1 and random.random() < move_def[4] and r+1 in rows and cells[r+1][c] == None:
+                cells[r+1][c] = cells[r][c]
+                cells[r][c] = None
+                return
+
 
 if __name__ == "__main__":
     main()
